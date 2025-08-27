10c885179724819eb898c6095e5e74e3d3d5e5cf
boosja <mathias.iversen487@gmail.com>
2025-08-27 08:18:16 +0200

    Add datasets for count of characters in subject and body per author

--- src/repolyzer/prepare.clj
+++ src/repolyzer/prepare.clj
@@ -2,6 +2,9 @@
   (:require [clojure.set :as set]
             [datomic.api :as d]))
 
+(defn entities [db entity-ids]
+  (map #(d/entity db %) entity-ids))
+
 (defn ->ld [inst]
   (java.time.LocalDate/ofInstant (.toInstant inst) (java.time.ZoneId/of "Europe/Oslo")))
 
@@ -15,7 +18,7 @@
               (let [d (->ld d)]
                 (str (.getYear d) "-" (.getMonthValue d)))))
        (frequencies)
-       vec))
+       seq))
 
 (defn sum-characters [stats subject body]
   (let [s-count (+ (or (:s-count stats) 0)
@@ -28,34 +31,51 @@
             (count subject)
             (count body))}))
 
-(defn character-stats [db]
-  (->> (d/q '[:find [?c ...]
-              :where
-              [?c :commit/full-hash ?hash]]
+(defn gather-char-stats [db]
+  (->> (d/q '[:find [?p ...]
+              :where [?p :person/email]]
             db)
-       (map #(d/entity db %))
-       (reduce (fn [character-stats commit]
-                 (let [stats (update character-stats
-                                     (-> commit :commit/committer :person/name)
-                                     #(sum-characters % (:commit/subject commit)
-                                                      (:commit/body commit)))]
-                   (if (:commit/co-authors commit)
-                     (reduce (fn [st co-a]
-                               (update st (:person/name co-a)
-                                       #(sum-characters % (:commit/subject commit)
-                                                        (:commit/body commit))))
-                             stats
-                             (:commit/co-authors commit))
-                     stats)))
-               {})))
+       (entities db)
+       (keep (fn [p] [(:person/name p) (set/union (:commit/_committer p)
+                                                  (:commit/_co-authors p))]))
+       (reduce (fn [res [name commits]]
+                 (assoc res name
+                        (reduce
+                         (fn [counts commit]
+                           (-> counts
+                               (update :antall-commits (fnil inc 0))
+                               (update :subj-chars (fnil + 0) (count (:commit/subject commit)))
+                               (update :body-chars (fnil + 0) (count (:commit/body commit)))))
+                         {} commits)))
+               {})
+       (map (fn [st]
+              (update st 1
+                      #(assoc %
+                              :subj-avg-chars (int (/ (:subj-chars %) (:antall-commits %)))
+                              :body-avg-chars (int (/ (:body-chars %) (:antall-commits %)))
+                              :char-sum (+ (:subj-chars %) (:body-chars %))))))
+       (into {})))
+
+(defn prepare-character-datasets [char-stats]
+  (reduce (fn [datasets [name stats]]
+            (reduce (fn [dset stat]
+                      (update dset (key stat) conj [name (val stat)]))
+                    datasets stats))
+          {}
+          char-stats))
 
 (defn prepare [db]
-  {:commits-per-month (commits-per-month db)
-   :character-stats (character-stats db)})
+  (-> {:commits-per-month (commits-per-month db)}
+      (merge (prepare-character-datasets (gather-char-stats db)))))
 
 (comment
 
   (def db (d/db (:conn @repolyzer.core/ctx)))
+
+  (prepare db)
+
+  (commits-per-month db)
+  (prepare-character-datasets (gather-char-stats db))
 
   (->> (d/q '[:find ?c ?name
               :where [?c :commit/committer ?name]]
